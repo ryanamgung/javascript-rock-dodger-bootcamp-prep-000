bb8fcd8524abe1a26ad7afa0f3b5e351337be46d
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-23 16:34:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -46,9 +46,8 @@ function createRock(x) {
 
   // Hmmm, why would we have used `var` here?
   var top = 0
-
   rock.style.top = top
-  GAME.appendChild(rock);
+  GAME.appendChild`${top}px`;
 
   /**
    * Now that we have a rock, we'll need to append
