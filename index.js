50cdcffad5730497376a940635068dddcd43de9a
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-23 16:31:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -130,10 +130,12 @@ function moveDodger(e) {
    */
   if (e.which === LEFT_ARROW) {
     e.stopPropagation();
+    e.preventDefault();
     moveDodgerLeft();
   }
   else if(e.which === RIGHT_ARROW){
     e.stopPropagation();
+    e.preventDefault();
     moveDodgerRight();
   }
 }
