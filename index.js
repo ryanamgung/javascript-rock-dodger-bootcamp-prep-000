48a2b5f643f9c52f546eb0550d77284b9d33110a
Ryan Namgung <heon.namgung@richmond.edu>
2018-05-23 16:56:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -31,10 +31,12 @@ function checkCollision(rock) {
     const rockLeftEdge = positionToInteger(rock.style.left)
     const rockRightEdge = rockLeftEdge + 20;
 
-    if((rockLeftEdge <= dodgerLeftEdge && rockRightEdge >= dodgerLeftEdge) ||
-      (rockLeftEdge >= dodgerLeftEdge && rockRightEdge <= dodgerRightEdge) ||
-      (rockLeftEdge <= dodgerRightEdge && rockRightEdge >= dodgerRightEdge))
+    if((rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerLeftEdge) ||
+      (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerRightEdge) ||
+      (rockLeftEdge > dodgerRightEdge && rockRightEdge < dodgerRightEdge))
       {
+        return false;
+      }else{
         return true;
       }
     }
